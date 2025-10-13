ef6d8266281d1445b12a749f3aa50f8a6a435c15
FDU_zflu <109411088+whitezb12@users.noreply.github.com>
2025-10-13 13:00:44 +0800

    Add files via upload

---
+++ mycode3/__init__.py
@@ -0,0 +1,9 @@
+import mycode.model
+import mycode.network
+import mycode.utils
+import mycode.smooth
+import mycode.metrics
+from .model import Model
+from .smooth import smooth_link_feat
+
+__version__ = '1.0.0'
