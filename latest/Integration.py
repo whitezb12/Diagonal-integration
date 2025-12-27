4c75f82d5d6e1fe44f9955c55794c1909f0ffd96
FDU_zflu <109411088+whitezb12@users.noreply.github.com>
2025-12-27 18:16:00 +0800

    Update Integration.py

--- latest/Integration.py
+++ latest/Integration.py
@@ -20,7 +20,7 @@ class IntegrationModel:
         input_key: List[Optional[str]] = ['X_pca', 'X_lsi'],
         batch_size: int = 500,
         training_steps: int = 10000,
-        warmups: int = 2000,
+        warmups: int = 4000,
         seed: int = 1234,
         n_latent: int = 10,
         cut_off: float = 0.9,
@@ -80,106 +80,80 @@ class IntegrationModel:
         self.dataloader_B = load_data(self.dataset_B, batch_size = self.batch_size, mode = "integration")
 
         self.model_path = model_path
-
-    def train(self) -> None:
+    
+    def train(self):
         self._init_models_and_optimizers()
         self._set_train_mode()
-        self.bm_loss = SharedBarycenterLoss(K=10, lamda_eigen=0.5, min_shared=64, warmup=self.warmups)
-        iterator_A, iterator_B = iter(self.dataloader_A), iter(self.dataloader_B)
+
         begin_time = time.time()
         print("Training started at:", time.asctime())
 
-        for step in range(self.training_steps):
-            batch_A, batch_B = next(iterator_A), next(iterator_B)
+        self._train_stage1()
+        self._train_stage2()
+        
+        self.train_time = time.time() - begin_time
+        print(f"Training finished. Time: {self.train_time:.2f} sec")
+
+        if self.model_path:
+            os.makedirs(self.model_path, exist_ok=True)
+            state = {
+                'E_A': self.E_A.state_dict(),
+                'E_B': self.E_B.state_dict(),
+                'G_A': self.G_A.state_dict(),
+                'G_B': self.G_B.state_dict(),
+            }
+            torch.save(state, os.path.join(self.model_path, "ckpt.pth"))
+    
+    def _train_stage1(self):
+
+        print("===== Stage 1: Warmup =====")
+
+        iterator_A = iter(self.dataloader_A)
+        iterator_B = iter(self.dataloader_B)
+
+        for step in range(self.warmups+1):
+
+            batch_A = next(iterator_A)
+            batch_B = next(iterator_B)
+
             x_A = batch_A['input'].float().to(self.device)
             x_B = batch_B['input'].float().to(self.device)
 
             z_A, mu_A, logvar_A = self.E_A(x_A)
             z_B, mu_B, logvar_B = self.E_B(x_B)
+
             sigma_A = torch.exp(0.5 * logvar_A)
             sigma_B = torch.exp(0.5 * logvar_B)
 
-            x_AtoB = self.G_B(mu_A)
-            x_BtoA = self.G_A(mu_B)
-
-            _, mu_AtoB, _ = self.E_B(x_AtoB)
-            _, mu_BtoA, _ = self.E_A(x_BtoA)
-
             x_Arecon = self.G_A(z_A)
-            x_Brecon = self.G_B(z_B)            
-            
-            Sim = pairwise_cosine(mu_A.detach(), mu_B.detach())
-            Sim = Sim / Sim.max()
-            max_A = Sim.max(dim=1).values 
-            max_B = Sim.max(dim=0).values
-            mask_A = (max_A > self.cut_off)
-            mask_B = (max_B > self.cut_off)
+            x_Brecon = self.G_B(z_B)
 
-            loss_dict = {}
+            _, mu_AtoB, _ = self.E_B(self.G_B(mu_A))
+            _, mu_BtoA, _ = self.E_A(self.G_A(mu_B))
 
             # input autoencoder loss
             beta = 0.01
             loss_AE_A = torch.mean((x_Arecon - x_A)**2) + beta * kl_divergence(mu_A, logvar_A)
             loss_AE_B = torch.mean((x_Brecon - x_B)**2) + beta * kl_divergence(mu_B, logvar_B)
-            loss_dict['AE'] = loss_AE_A + loss_AE_B
+            loss_AE = loss_AE_A + loss_AE_B
 
             # latent align loss
             loss_LA_AtoB = torch.mean((mu_A - mu_AtoB)**2) 
             loss_LA_BtoA = torch.mean((mu_B - mu_BtoA)**2) 
-            loss_dict['LA'] = loss_LA_AtoB + loss_LA_BtoA       
+            loss_LA = loss_LA_AtoB + loss_LA_BtoA 
 
             # optimal transport process
             C = pairwise_correlation_distance(batch_A['link_feat'], batch_B['link_feat']).to(self.device)
-            P = unbalanced_ot(cost_pp=C, reg=0.05, reg_m=0.5, device=self.device)  
+            P = unbalanced_ot(C, reg=0.05, reg_m=0.5, device=self.device)
 
             # distribution alignment loss
             z_dist = pairwise_euclidean_distance(mu_A, mu_B) + pairwise_euclidean_distance(sigma_A, sigma_B)
-            loss_dict['DA'] = torch.sum(P * z_dist) / torch.sum(P)
+            loss_DA = torch.sum(P * z_dist) / torch.sum(P)
 
-            # Barycenter Mapping loss
-            loss_dict['BM'] = self.bm_loss(C, z_A, z_B, mask_A, mask_B, step)
-
-            # discriminator loss
-            margin = 1.0
-            for _ in range(3): 
-                self.optimizer_Dis_m.zero_grad() 
-                loss_mDis_A = (F.softplus(-torch.clamp(self.Dis_Z(z_A[mask_A].detach()), -margin, margin))).mean()
-                loss_mDis_B = (F.softplus(torch.clamp(self.Dis_Z(z_B[mask_B].detach()), -margin, margin))).mean()
-                alpha = min(1.0, max(0.0, (step - self.warmups) / 500))
-                loss_dict['mDis'] = alpha * (loss_mDis_A + loss_mDis_B)
-                loss_dict['mDis'].backward()
-                self.optimizer_Dis_m.step()
-
-            if self.optimizer_Dis_b:
-                self.optimizer_Dis_b.zero_grad()
-                loss_dict['bDis'] = self.compute_discriminator_loss_intra(z_A.detach(), z_B.detach(), batch_A, batch_B)
-                loss_dict['bDis'].backward()
-                self.optimizer_Dis_b.step()
-            else:
-                loss_dict['bDis'] = torch.tensor(0.0, device=self.device)
-
-            # generator loss
-            loss_mGAN_A = -(F.softplus(-torch.clamp(self.Dis_Z(z_A[mask_A]), -margin, margin))).mean()
-            loss_mGAN_B = -(F.softplus(torch.clamp(self.Dis_Z(z_B[mask_B]), -margin, margin))).mean()
-            alpha = min(1.0, max(0.0, (step - self.warmups) / 500))
-            loss_dict['mGAN'] = alpha * (loss_mGAN_A + loss_mGAN_B) 
-            loss_dict['bGAN'] = -self.compute_discriminator_loss_intra(z_A, z_B, batch_A, batch_B)           
-            
-            # semi-supervised clip loss(optional)
-            if self.use_prior:
-                prior_matrix = build_celltype_prior(batch_A['celltype'], batch_B['celltype']).to(self.device)
-                loss_dict['CLIP'] = generalized_clip_loss_stable_masked(z_A, z_B, prior_matrix)
-            else:
-                loss_dict['CLIP'] = torch.tensor(0.0, device=self.device)
-            
             total_loss = (
-                self.lambdaRecon * loss_dict['AE']
-                + self.lambdaLA * loss_dict['LA']
-                + self.lambdaDA * loss_dict['DA']
-                + self.lambdaBM * loss_dict['BM']
-                + self.lambdamGAN * loss_dict['mGAN']
-                + self.lambdabGAN * loss_dict['bGAN']
-                + self.lambdaCLIP * loss_dict['CLIP']
+                self.lambdaRecon * loss_AE
+                + self.lambdaLA * loss_LA
+                + self.lambdaDA * loss_DA
             )
 
             self.optimizer_G.zero_grad()
@@ -187,23 +161,147 @@ class IntegrationModel:
             torch.nn.utils.clip_grad_norm_(self.params_G, 5.0)
             self.optimizer_G.step()
 
-            if step % 1000 == 0:
-                self.log(step, loss_dict)
+            if step % 500 == 0:
+                print(f"[Stage1 {step}] AE: {loss_AE:.4f} | LA: {loss_LA:.4f} | DA: {loss_DA:.4f}")
 
-        self.train_time = time.time() - begin_time
-        print(f"Training finished. Time: {self.train_time:.2f} sec")
 
-        if self.model_path:
-            os.makedirs(self.model_path, exist_ok=True)
+    def _train_stage2(self):
 
-            state = {
-                'E_A': self.E_A.state_dict(),
-                'E_B': self.E_B.state_dict(),
-                'G_A': self.G_A.state_dict(),
-                'G_B': self.G_B.state_dict(),
-            }
+        print("===== Stage 2: Shared Alignment =====")
 
-            torch.save(state, os.path.join(self.model_path, "ckpt.pth"))
+        iterator_A = iter(self.dataloader_A)
+        iterator_B = iter(self.dataloader_B)
+
+        for step in range(self.warmups+1, self.training_steps+1):
+
+            batch_A = next(iterator_A)
+            batch_B = next(iterator_B)
+
+            x_A = batch_A['input'].float().to(self.device)
+            x_B = batch_B['input'].float().to(self.device)
+
+            z_A, mu_A, logvar_A = self.E_A(x_A)
+            z_B, mu_B, logvar_B = self.E_B(x_B)
+
+            sigma_A = torch.exp(0.5 * logvar_A)
+            sigma_B = torch.exp(0.5 * logvar_B)
+
+            x_Arecon = self.G_A(z_A)
+            x_Brecon = self.G_B(z_B)
+
+            _, mu_AtoB, _ = self.E_B(self.G_B(mu_A))
+            _, mu_BtoA, _ = self.E_A(self.G_A(mu_B))            
+            
+            # shared gate 
+            Sim = pairwise_cosine(mu_A.detach(), mu_B.detach())
+            Sim = Sim / Sim.max()
+            mask_A = Sim.max(dim=1).values > self.cut_off
+            mask_B = Sim.max(dim=0).values > self.cut_off            
+            
+            min_shared = 50
+            if mask_A.sum() < min_shared or mask_B.sum() < min_shared:
+                continue
+
+            # input autoencoder loss
+            beta = 0.01
+            loss_AE_A = torch.mean((x_Arecon - x_A)**2) + beta * kl_divergence(mu_A, logvar_A)
+            loss_AE_B = torch.mean((x_Brecon - x_B)**2) + beta * kl_divergence(mu_B, logvar_B)
+            loss_AE = loss_AE_A + loss_AE_B
+
+            # latent align loss
+            loss_LA_AtoB = torch.mean((mu_A - mu_AtoB)**2) 
+            loss_LA_BtoA = torch.mean((mu_B - mu_BtoA)**2) 
+            loss_LA = loss_LA_AtoB + loss_LA_BtoA             
+            
+            z_A_s = z_A[mask_A]
+            z_B_s = z_B[mask_B]
+
+            # # input autoencoder loss
+            # beta = 0.01
+            # loss_AE_A = torch.mean((x_Arecon[mask_A] - x_A[mask_A])**2) + beta * kl_divergence(mu_A[mask_A], logvar_A[mask_A])
+            # loss_AE_B = torch.mean((x_Brecon[mask_B] - x_B[mask_B])**2) + beta * kl_divergence(mu_B[mask_B], logvar_B[mask_B])
+            # loss_AE = loss_AE_A + loss_AE_B
+
+            # # latent align loss
+            # loss_LA_AtoB = torch.mean((mu_A[mask_A] - mu_AtoB[mask_A])**2) 
+            # loss_LA_BtoA = torch.mean((mu_B[mask_B] - mu_BtoA[mask_B])**2) 
+            # loss_LA = loss_LA_AtoB + loss_LA_BtoA             
+            
+            # z_A_s = z_A[mask_A]
+            # z_B_s = z_B[mask_B]
+            # optimal transport process(shared only)
+            C = pairwise_correlation_distance(batch_A['link_feat'], batch_B['link_feat']).to(self.device)
+            C_s = C[mask_A][:, mask_B]
+            P_s = unbalanced_ot(cost_pp=C_s, reg=0.05, reg_m=0.5, device=self.device)  
+
+            # distribution alignment loss(shared onbly)
+            z_dist_s = pairwise_euclidean_distance(mu_A[mask_A], mu_B[mask_B]) + pairwise_euclidean_distance(sigma_A[mask_A], sigma_B[mask_B])
+            loss_DA = torch.sum(P_s * z_dist_s) / torch.sum(P_s)
+
+            # Barycenter Mapping loss
+            L_A = Graph_Laplacian_torch(z_A_s, nearest_neighbor=min(10, z_A_s.size(0) - 1))
+            L_B = Graph_Laplacian_torch(z_B_s, nearest_neighbor=min(10, z_B_s.size(0) - 1))
+            z_A_new = Transform(z_A_s, z_B_s, P_s, L_A, lamda_Eigenvalue=0.5)
+            z_B_new = Transform(z_B_s, z_A_s, P_s.t(), L_B, lamda_Eigenvalue=0.5)
+            loss_BM = torch.mean((z_A_s - z_A_new) ** 2) + torch.mean((z_B_s - z_B_new) ** 2)
+
+            # discriminator loss
+            margin = 5.0
+            for _ in range(3): 
+                self.optimizer_Dis_m.zero_grad() 
+                loss_mDis_A = (F.softplus(-torch.clamp(self.Dis_Z(z_A_s.detach()), -margin, margin))).mean()
+                loss_mDis_B = (F.softplus(torch.clamp(self.Dis_Z(z_B_s.detach()), -margin, margin))).mean()
+                loss_mDis = loss_mDis_A + loss_mDis_B
+                loss_mDis.backward()
+                self.optimizer_Dis_m.step()
+
+            if self.optimizer_Dis_b:
+                self.optimizer_Dis_b.zero_grad()
+                loss_bDis = self.compute_discriminator_loss_intra(z_A.detach(), z_B.detach(), batch_A, batch_B)
+                loss_bDis.backward()
+                self.optimizer_Dis_b.step()
+            else:
+                loss_bDis = torch.tensor(0.0, device=self.device)
+
+            # generator loss
+            loss_mGAN_A = -(F.softplus(-torch.clamp(self.Dis_Z(z_A_s), -margin, margin))).mean()
+            loss_mGAN_B = -(F.softplus(torch.clamp(self.Dis_Z(z_B_s), -margin, margin))).mean()
+            loss_mGAN = loss_mGAN_A + loss_mGAN_B
+            loss_bGAN = -self.compute_discriminator_loss_intra(z_A, z_B, batch_A, batch_B)           
+            
+            # semi-supervised clip loss(optional)
+            if self.use_prior:
+                prior_matrix = build_celltype_prior(batch_A['celltype'], batch_B['celltype']).to(self.device)
+                loss_CLIP = generalized_clip_loss_stable_masked(z_A, z_B, prior_matrix)
+            else:
+                loss_CLIP = torch.tensor(0.0, device=self.device)
+
+            total_loss = (
+                self.lambdaRecon * loss_AE
+                + self.lambdaLA * loss_LA
+                + self.lambdaDA * loss_DA
+                + self.lambdaBM * loss_BM
+                + self.lambdaCLIP * loss_CLIP
+                + self.lambdamGAN * loss_mGAN
+                + self.lambdabGAN * loss_bGAN
+            )
+
+            self.optimizer_G.zero_grad()
+            total_loss.backward()
+            torch.nn.utils.clip_grad_norm_(self.params_G, 5.0)
+            self.optimizer_G.step()
+
+            if step % 500 == 0:
+                print(
+                    f"[Stage2 {step}] "
+                    f"AE: {loss_AE:.4f} | "
+                    f"LA: {loss_LA:.4f} | "
+                    f"DA: {loss_DA:.4f} | "
+                    f"BM: {loss_BM:.4f} | "
+                    f"CLIP: {loss_CLIP:.4f} | "
+                    f"mGAN: {loss_mGAN:.4f} | "
+                    f"bGAN: {loss_bGAN:.4f}"
+                )
         
 
     def get_latent_representation(self) -> None:
@@ -321,55 +419,7 @@ class IntegrationModel:
             if model is not None:
                 model.eval()
 
-    def log(self, step: int, loss_dict: Dict[str, torch.Tensor]) -> None:
-        print(
-            f"Step {step} | "
-            f"loss_Recon: {self.lambdaRecon * loss_dict['AE']:.4f} | "
-            f"loss_LA: {self.lambdaLA * loss_dict['LA']:.4f} | "
-            f"loss_DA: {self.lambdaDA * loss_dict['DA']:.4f} | "
-            f"loss_BM: {self.lambdaBM * loss_dict['BM']:.4f} | "
-            f"loss_CLIP: {self.lambdaCLIP * loss_dict['CLIP']:.4f} | "
-            f"loss_mGAN: {loss_dict['mGAN']:.4f}| "
-            f"loss_bGAN: {loss_dict['bGAN']:.4f}"
-        )
 
 
 
-class SharedBarycenterLoss(torch.nn.Module):
-    def __init__(
-        self,
-        K=10,
-        lamda_eigen=0.5,
-        min_shared=64,
-        warmup=2000,
-    ):
-        super().__init__()
-        self.K = K
-        self.lamda_eigen = lamda_eigen
-        self.min_shared = min_shared
-        self.warmup = warmup
-
-    def forward(self, C, z_A, z_B, mask_A, mask_B, step):
-        device = z_A.device
-
-        if step < self.warmup:
-            return torch.tensor(0.0, device=device)
-
-        if mask_A.sum() < self.min_shared or mask_B.sum() < self.min_shared:
-            return torch.tensor(0.0, device=device)
-
-        z_A_s = z_A[mask_A]
-        z_B_s = z_B[mask_B]
-
-        C_s = C[mask_A][:, mask_B]
-        P_s = unbalanced_ot(cost_pp=C_s, reg=0.05, reg_m=0.5, device=device)  
-
-        L_A = Graph_Laplacian_torch(z_A_s, nearest_neighbor=min(self.K, z_A_s.size(0) - 1))
-        L_B = Graph_Laplacian_torch(z_B_s, nearest_neighbor=min(self.K, z_B_s.size(0) - 1))
-
-        z_A_new = Transform(z_A_s, z_B_s, P_s, L_A, lamda_Eigenvalue=self.lamda_eigen)
-        z_B_new = Transform(z_B_s, z_A_s, P_s.t(), L_B, lamda_Eigenvalue=self.lamda_eigen)
-        loss = torch.mean((z_A_s - z_A_new) ** 2) + torch.mean((z_B_s - z_B_new) ** 2)
-
-        return loss
 
